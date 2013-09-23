17ff5c6da62588af87a87292a72958370d7655ce
Mathieu <mathieu@redapesolutions.com>
2013-09-23 12:12:07 +0800

    One less # for API

--- api/templates/api.js
+++ api/templates/api.js
@@ -1,4 +1,4 @@
-// #####<a id="<%= generalAnswers.id %>""><%= generalAnswers.url %></a>
+// ####<a id="<%= generalAnswers.id %>""><%= generalAnswers.url %></a>
 // <%= generalAnswers.METHOD %>
 // <%= generalAnswers.description %>
 //
